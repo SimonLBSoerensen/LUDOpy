5eee85eaaf67607f145ef8f1e2ece2497bf88bb1
SimonLBSoerensen <simonlyckbjaert@hotmail.com>
2020-04-05 00:02:05 +0200

    1.1.2: Most just docs

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2020, Simon Lyck Bjært Sørensen'
 author = 'Simon Lyck Bjært Sørensen'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.1'
+release = '1.1.2'
 
 
 # -- General configuration ---------------------------------------------------
