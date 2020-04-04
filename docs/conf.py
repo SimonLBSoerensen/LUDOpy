4ad44b27e7ac90fd6bf7cdcf83e76db29a93b2a5
SimonLBSoerensen <simonlyckbjaert@hotmail.com>
2020-04-04 22:37:05 +0200

    Docs

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ master_doc = 'index'
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
-
+import sphinx_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -50,8 +50,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
-
+#html_theme = 'alabaster'
+html_theme = "neo_rtd_theme"
+html_theme_path = [sphinx_theme.get_html_theme_path('stanford-theme')]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
