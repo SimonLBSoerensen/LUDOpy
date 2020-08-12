707fdb7ef5aa5a6b4ffe3f8648c8776eccb558d5
SimonLBSoerensen <simonlyckbjaert@hotmail.com>
2020-08-12 23:48:24 +0200

    Ghost players added -> pypi 1.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ludopy",
-    version="1.2.7",
+    version="1.3.0",
     author="Simon L. B. Sørensen",
     author_email="simonlyckbjaert@hotmail.com",
     description="LUDO python",
